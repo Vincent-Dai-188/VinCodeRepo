53fd96e4aada5eb2dbcb689938b48f13733bb5b8
Vincent Dai <vincentdai188@gmail.com>
2017-07-25 16:14:30 +0800

    Add files via upload

---
+++ socket_linux/tcp_client.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <errno.h>
+
+#define PORT 8888                                /*侦听端口地址*/
+
+extern void process_conn_client(int s);
+
+int main(int argc, char *argv[])
+{
+    int s;                                       /*s为socket描述符*/
+    struct sockaddr_in server_addr;              /*服务器地址结构*/
+	char server_ip_str[INET_ADDRSTRLEN];
+    
+    s = socket(AF_INET, SOCK_STREAM, 0);         /*建立一个流式套接字 */
+    if(s < 0) {                                  /*出错*/
+        printf("socket error\n");
+        return -1;
+    }    
+
+    /*设置服务器地址*/
+    bzero(&server_addr, sizeof(server_addr));           /*清零*/
+    server_addr.sin_family = AF_INET;                   /*协议族*/
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);    /*本地地址*/
+    server_addr.sin_port = htons(PORT);                 /*服务器端口*/
+    
+    /*将用户输入的字符串类型的IP地址转为整型*/
+    inet_pton(AF_INET, argv[1], &server_addr.sin_addr);
+
+    /*连接服务器*/
+    connect(s, (struct sockaddr*)&server_addr, sizeof(struct sockaddr));
+	//Convert IPv4 and IPv6 addresses from binary to text form
+    if (!inet_ntop(AF_INET, &(server_addr.sin_addr), server_ip_str, sizeof(server_ip_str))) {
+        perror("inet_ntop failed");
+        exit(EXIT_FAILURE);
+    }
+	printf("*** Connected to server '%s:%d' ***\n", server_ip_str, PORT);
+    process_conn_client(s);                      /*客户端处理过程*/
+    close(s);                                    /*关闭连接*/
+    return 0;
+}
---
+++ socket_linux/tcp_process.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+/*客户端的处理过程*/
+void process_conn_client(int s)
+{
+    ssize_t size = 0;
+    char buffer[1024];                            /*数据的缓冲区*/
+    
+    for(;;) {                                     /*循环处理过程*/
+        /*从标准输入中读取数据放到缓冲区buffer中*/
+        size = read(0, buffer, 1024);
+        if(size > 0){                             /*读到数据*/
+            write(s, buffer, size);               /*发送给服务器*/
+            size = read(s, buffer, 1024);         /*从服务器读取数据*/
+            write(1, buffer, size);               /*写到标准输出*/
+        }
+    }
+}
+
+/*服务器对客户端的处理*/
+void process_conn_server(int s)
+{
+    ssize_t size = 0;
+    char buffer[1024];                            /*数据的缓冲区*/
+    char out_buffer[1024];						  /*show buffer*/
+
+    for(;;) {                                     /*循环处理过程*/        
+        size = read(s, buffer, 1024);             /*从套接字中读取数据放到缓冲区buffer中*/
+        if(size == 0) {                           /*没有数据*/
+		    /*从标准输入中读取数据放到缓冲区out_buffer中*/
+		    size = read(0, out_buffer, 1024);
+		    if(size > 0){                             /*读到数据*/
+		        write(s, out_buffer, size);           /*发送给客户端*/
+		    }
+            return;    
+        }
+        
+		sprintf(out_buffer, "[From client_%d]: %s\n", s, buffer);
+        //write(1, buffer, size);                 /*写到标准输出*/
+        write(1, out_buffer, strlen(out_buffer) + 1);          /*写到标准输出*/
+
+        sprintf(buffer, "[From Server]: Received %d bytes.\n", size);
+        write(s, buffer, strlen(buffer)+1);       /*发给客户端*/
+    }    
+}
